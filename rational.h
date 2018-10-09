e3fa787846ac718f491734b62bbe5df3c4754a32
gena bug <archaerolog@gmail.com>
2018-10-09 14:53:10 +0300

    added stubs for rational type

---
+++ rational.h
@@ -0,0 +1,12 @@
+#include <iosfwd>
+
+class rational
+{
+    // ...
+public:
+    rational(int num = 0, int den = 1) { /*...*/ }
+    int num() const { return -1; }
+    int den() const { return -1; }
+    // ...
+};
+
